58827a1d300d3d32238f4fc65dc6a2fcd59750aa
zgreer <56371174+zgreer@users.noreply.github.com>
2021-11-10 20:27:32 -0500

    Update lahman_build.R

---
+++ lahman_build.R
@@ -0,0 +1,117 @@
+library(tidyverse);
+
+career_batting=function(year, regular_season=TRUE) {
+  
+  if(regular_season==TRUE) {
+    Lahman::Batting %>% replace(is.na(.), 0) %>% 
+      filter(lgID %in% c("AL", "NL") & yearID>year) %>% 
+      transmute(playerID, season=yearID, level="MLB", league=lgID, team=teamID, 
+                order=paste0("R", "-", as.character(stint)), 
+                g=G, pa=(H+BB+SO+HBP+SH+SF), ab=AB, h=H, db=X2B, tr=X3B, 
+                hr=HR, r=R, rbi=RBI, k=SO, bb=BB, bb=IBB, ubb=BB-IBB, 
+                hbp=HBP, sh=SH, sf=SF, gidp=GIDP, sb=SB, cs=CS)
+  }
+  
+  else{
+    Lahman::BattingPost %>% replace(is.na(.), 0) %>% 
+      filter(lgID %in% c("AL", "NL") & yearID>year) %>% 
+      transmute(playerID, season=yearID, level="MLB", league=lgID, team=teamID, 
+                order=round, g=G, pa=(H+BB+SO+HBP+SH+SF), ab=AB, h=H, db=X2B, 
+                tr=X3B, hr=HR, r=R, rbi=RBI, k=SO, bb=BB, bb=IBB, ubb=BB-IBB, 
+                hbp=HBP, sh=SH, sf=SF, gidp=GIDP, sb=SB, cs=CS)
+  }
+  
+};
+modern_era_batting=function(year=1960){
+  df1=career_batting(year=year)
+  list=unique(df1$playerID)
+  df2=career_batting(year=min((Lahman::Batting)$yearID)) %>% 
+    filter(playerID %in% list)
+  df2=df2 %>% anti_join(df1, 
+                        by=c("playerID", "season", "level", 
+                             "league", "team", "order"));
+  
+  df3=career_batting(year=year, regular_season=FALSE)
+  list=unique(df3$playerID)
+  df4=career_batting(year=min((Lahman::BattingPost)$yearID), 
+                     regular_season=FALSE) %>% filter(playerID %in% list)
+  df4=df4 %>% anti_join(df3, 
+                        by=c("playerID", "season", "level", 
+                             "league", "team", "order"))
+  
+  playoff_order=function(df){
+    c(sort(unique(df$order)[str_detect(unique(df$order), "R")]),
+      sort(unique(df$order)[str_detect(unique(df$order), "WC")]),
+      sort(unique(df$order)[str_detect(unique(df$order), "DIV")]),
+      sort(unique(df$order)[str_detect(unique(df$order), "DS")]),
+      sort(unique(df$order)[str_detect(unique(df$order), "CS")]), "WS")
+  }
+  
+  df_final=rbind(df1, df2, df3, df4) %>% mutate(order=as.character(order))
+  
+  df_final %>% mutate(order=factor(order)) %>% 
+    mutate(order=fct_relevel(order, playoff_order(df_final))) %>% 
+    arrange(playerID, season, level, league, team, order)
+  
+};
+df_career_batting=modern_era_batting();
+
+career_pitching=function(year, regular_season=TRUE) {
+  
+  if(regular_season==TRUE) {
+    Lahman::Pitching %>% replace(is.na(.), 0) %>% 
+      filter(lgID %in% c("AL", "NL") & yearID>year) %>% 
+      transmute(playerID, season=yearID, level="MLB", league=lgID, team=teamID, 
+                order=stint, g=G, gs=GS, w=W, l=L, 
+                ip=as.numeric(paste0(floor(IPouts/3), ".", IPouts%%3)), 
+                r=R, er=ER, h=H, hr=HR, k=SO, bb=BB, ibb=IBB, ubb=BB-IBB, 
+                hbp=HBP, wp=WP, bk=BK, sh=SH, sf=SF, gidp=GIDP, bf=BFP, 
+                cg=CG, sho=SHO, gf=GF, sv=SV)
+  }
+  
+  else{
+    Lahman::PitchingPost %>% replace(is.na(.), 0) %>% 
+      filter(lgID %in% c("AL", "NL") & yearID>year) %>% 
+      transmute(playerID, season=yearID, level="MLB", league=lgID, team=teamID, 
+                order=round, g=G, gs=GS, w=W, l=L, 
+                ip=as.numeric(paste0(floor(IPouts/3), ".", IPouts%%3)), 
+                r=R, er=ER, h=H, hr=HR, k=SO, bb=BB, ibb=IBB, ubb=BB-IBB, 
+                hbp=HBP, wp=WP, bk=BK, sh=SH, sf=SF, gidp=GIDP, bf=BFP, 
+                cg=CG, sho=SHO, gf=GF, sv=SV)
+  }
+  
+};
+modern_era_pitching=function(year=1960){
+  df1=career_pitching(year=year)
+  list=unique(df1$playerID)
+  df2=career_pitching(year=min((Lahman::Pitching)$yearID)) %>% 
+    filter(playerID %in% list)
+  df2=df2 %>% anti_join(df1, 
+                        by=c("playerID", "season", "level", 
+                             "league", "team", "order"));
+  
+  df3=career_pitching(year=year, regular_season=FALSE)
+  list=unique(df3$playerID)
+  df4=career_pitching(year=min((Lahman::PitchingPost)$yearID), 
+                     regular_season=FALSE) %>% filter(playerID %in% list)
+  df4=df4 %>% anti_join(df3, 
+                        by=c("playerID", "season", "level", 
+                             "league", "team", "order"))
+  
+  playoff_order=function(df){
+    c(sort(unique(df$order)[str_detect(unique(df$order), "R")]),
+      sort(unique(df$order)[str_detect(unique(df$order), "WC")]),
+      sort(unique(df$order)[str_detect(unique(df$order), "DIV")]),
+      sort(unique(df$order)[str_detect(unique(df$order), "DS")]),
+      sort(unique(df$order)[str_detect(unique(df$order), "CS")]), "WS")
+  }
+  
+  df_final=rbind(df1, df2, df3, df4) %>% mutate(order=as.character(order))
+  
+  df_final %>% mutate(order=factor(order)) %>% 
+    mutate(order=fct_relevel(order, playoff_order(df_final))) %>% 
+    arrange(playerID, season, level, league, team, order)
+  
+};
+df_career_pitching=modern_era_pitching();
+
