72baed11c972084611351f150b25567562300917
zgreer <56371174+zgreer@users.noreply.github.com>
2021-11-14 19:13:25 -0500

    Create qry_02_retrosheet_lineups.R

---
+++ qry_02_retrosheet_lineups.R
@@ -0,0 +1,64 @@
+library(tidyverse);
+setwd("~/Desktop/github/baseball-database")
+
+
+##breaks down str_extract_all output list and returns a string of all matches
+unpack=function(string, look_for){
+  
+  sapply(str_extract_all(string, look_for), function(x) paste(x, collapse=""))
+  
+};
+
+
+##cuts down as.data.frame because i'm lazy
+df_=function(df){as.data.frame(df)};
+
+
+##returns the selected game list from getRetrosheet output
+retro_list=function(year, team){
+  
+  retrosheet::getRetrosheet("play", year, team)
+  
+};
+
+
+##declaring team list
+teams=retrosheet::getTeamIDs(2020)
+
+##writes 
+team_list=retro_list(2020, "MIL");
+##df_lineups=NULL;
+for(i in 1:length(team_list)){
+  
+  if(is.null(team_list[[i]]$sub)) {
+    
+    df_game_lineup=cbind(df_(team_list[[i]]$id) %>% drop_na(), 
+                         df_(team_list[[i]]$start) %>% drop_na()) %>% 
+      transmute(game_id=df, lineup="start", retroID, 
+                team, batPos, fieldPos) %>% 
+      left_join(df_(team_list[[i]]$data) %>% drop_na())
+    
+  }
+  
+  else{
+    
+    df_game_lineup=rbind(cbind(df_(team_list[[i]]$id) %>% drop_na(), 
+                               df_(team_list[[i]]$start) %>% drop_na()) %>% 
+                           transmute(game_id=df, lineup="start", retroID, 
+                                     team, batPos, fieldPos), 
+                         cbind(df_(team_list[[i]]$id) %>% drop_na(), 
+                               df_(team_list[[i]]$sub) %>% drop_na()) %>% 
+                           transmute(game_id=df, lineup="sub", retroID, team, 
+                                     batPos, fieldPos)) %>% 
+      arrange(team) %>% 
+      left_join(df_(team_list[[i]]$data) %>% drop_na())
+    
+  }
+  
+  df_lineups=rbind(df_lineups, df_game_lineup)
+  rm(i, df_game_lineup)
+  
+};
+
+
+write.csv(df_lineups, "lineups_2020.csv", row.names=FALSE)
